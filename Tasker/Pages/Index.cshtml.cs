c0b92d0965bb6f8f9c4049dcb15b0a81f9ea15e4
sam grimmer <samg@iqualify.com>
2020-05-15 20:31:25 +1200

    creating the layout and navigation

---
+++ Tasker/Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace Tasker.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public IndexModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
